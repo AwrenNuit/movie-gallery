ec8d1c977feaea180ae2b600d421d977f3606be9
AwrenNuit <a.rennaker@yahoo.com>
2020-01-03 13:59:23 -0600

    removed PUT from genre add and delete sagas

--- src/redux/sagas/deleteJunctionSaga.js
+++ src/redux/sagas/deleteJunctionSaga.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
-import {put} from 'redux-saga/effects';
 
 // Saga to DELETE a genre from selected film
 function* deleteJunctionSaga(action){
   try{
     yield axios.delete(`/film/junction`, {data: action.payload});
-    yield put({type: `GET_FILM`});
   }
   catch(error){
     console.log('error in DELETE new junction:', error);
--- src/redux/sagas/postJunctionSaga.js
+++ src/redux/sagas/postJunctionSaga.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
-import {put} from 'redux-saga/effects';
 
 // Saga to POST new genre
 function* postJunctionSaga(action){
   try{
     yield axios.post(`/film/junction`, action.payload);
-    yield put({type: `GET_FILM`});
   }
   catch(error){
     console.log('error in POST new junction:', error);
